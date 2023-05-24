d66dc51651faa3d48cf5adc69a264cda81d0e874
Chris <chris.clark@coffeeb4code.com>
2023-05-24 16:57:52 -0500

    need to see if this works by going through a simple block

--- cranelift/src/lib.rs
+++ cranelift/src/lib.rs
@@ -1,11 +1,90 @@
-use cranelift::frontend::FunctionBuilderContext;
+use cranelift::codegen::ir::*;
+use cranelift::codegen::isa::*;
+use cranelift::frontend::*;
+use cranelift::prelude::types::*;
 
-#[no_mangle]
-pub extern "C" fn cl_function_builder_context_new() -> *mut FunctionBuilderContext {
-    return &mut FunctionBuilderContext::new();
+macro_rules! empty_new {
+    ($attr:ident) => {
+        paste::paste! {
+            #[no_mangle]
+            pub extern "C" fn [<CL_ $attr _New >]() -> *mut [< $attr >] {
+                let mut val = [< $attr >]::new();
+                return &mut val;
+            }
+        }
+    }
 }
 
-#[no_mangle]
-pub extern "C" fn cl_function_builder_context_dispose(val: *mut FunctionBuilderContext) -> () {
-    core::mem::drop(val);
+macro_rules! empty_invoke_return {
+    ($attr:ident, $name:ident, $ret:ident) => {
+        paste::paste! {
+            #[no_mangle]
+            pub extern "C" fn [<CL_ $attr _ $name>](val: *mut $attr) -> *mut [< $ret >] {
+                let mut uval = unsafe { core::ptr::read(val) };
+                return &mut uval.[< $name >]();
+            }
+        }
+    }
 }
+
+macro_rules! empty_dispose {
+    ($attr:ident) => {
+        paste::paste! {
+            #[no_mangle]
+            pub extern "C" fn [<CL_ $attr _Dispose >](val: *mut [< $attr >]) -> () {
+                core::mem::drop(val);
+            }
+        }
+    }
+}
+
+macro_rules! type_new {
+    ($attr:ident, $name: ident, $($variant:tt)*) => {
+        paste::paste! {
+            #[no_mangle]
+            pub extern "C" fn [<CL_ $attr _New_ $name >]() -> *mut [< $attr >] {
+                let mut val = [< $attr >]::new($($variant)*);
+                return &mut val;
+            }
+        }
+    }
+}
+
+// ABIPARAM
+type_new!(AbiParam, I32, I32);
+
+// SIGNATURE
+type_new!(Signature, SystemV, CallConv::SystemV);
+empty_dispose!(Signature);
+
+// FUNCTION
+empty_new!(Function);
+empty_dispose!(Function);
+#[no_mangle]
+pub extern "C" fn CL_Function_With_Name_Signature(
+    name: *const UserFuncName,
+    sig: *const Signature,
+) -> *mut Function {
+    let uname = unsafe { core::ptr::read(name) };
+    let usig= unsafe { core::ptr::read(sig) };
+    return &mut Function::with_name_signature(uname, usig);
+
+}
+
+// FUNCTION BUILDER
+empty_dispose!(FunctionBuilder);
+empty_invoke_return!(FunctionBuilder, create_block, Block);
+
+#[no_mangle]
+pub extern "C" fn CL_FunctionBuilder_New<'a>(
+    function: *mut Function,
+    value: *mut FunctionBuilderContext,
+) -> *mut FunctionBuilder<'a> {
+    let ufunction = unsafe { &mut *function };
+    let uvalue = unsafe { &mut *value };
+    return &mut FunctionBuilder::new(ufunction, uvalue);
+}
+
+// FUNCTION BUILDER CONTEXT
+empty_new!(FunctionBuilderContext);
+empty_dispose!(FunctionBuilderContext);
--- src/cranelift-bindings.zig
+++ src/cranelift-bindings.zig
@@ -1,12 +1,43 @@
-pub const FContext = opaque {
-    pub const new = cl_function_builder_context_new;
-    extern fn cl_function_builder_context_new() *FContext;
+const std = @import("std");
+pub const UserFuncName = opaque {};
+pub const FunctionStencil = opaque {};
+pub const FunctionParameters = opaque {};
 
-    pub const dispose = cl_function_builder_context_dispose;
-    extern fn cl_function_builder_context_dispose(*FContext) void;
+pub const Signature = opaque {};
+pub const Function = opaque {
+    pub const named = cl_function_with_name_signature;
+    extern fn cl_function_with_name_signature(*UserFuncName, *Signature) *Function;
+
+    pub const anonymous = CL_Function_New;
+    extern fn CL_Function_New() *Function;
+
+    pub const dispose = CL_Function_Dispose;
+    extern fn CL_Function_Dispose(*Function) void;
+};
+
+pub const FunctionBuilder = opaque {
+    pub const new = cl_function_builder_new;
+    extern fn cl_function_builder_new(*Function, *Context) *FunctionBuilder;
+
+    pub const dispose = CL_FunctionBuilder_Dispose;
+    extern fn CL_FunctionBuilder_Dispose(*FunctionBuilder) void;
+};
+
+pub const Context = opaque {
+    pub const new = cl_function_builder_context_new;
+    extern fn cl_function_builder_context_new() *Context;
+
+    pub const dispose = CL_FunctionBuilderContext_Dispose;
+    extern fn CL_FunctionBuilderContext_Dispose(*Context) void;
 };
 
 test "should create and dispose context" {
-    const context = FContext.new();
-    defer FContext.dispose(context);
+    const context = Context.new();
+    defer Context.dispose(context);
+
+    const func = Function.anonymous();
+    defer Function.dispose(func);
+
+    const func_builder = FunctionBuilder.new(func, context);
+    defer FunctionBuilder.dispose(func_builder);
 }
