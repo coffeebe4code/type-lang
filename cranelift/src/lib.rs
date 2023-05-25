e1319acf6bf68b65b6b8fdd6e5e086e4198a7fa5
Chris <chris.clark@coffeeb4code.com>
2023-05-24 22:13:27 -0500

    many macros to come

--- cranelift/src/lib.rs
+++ cranelift/src/lib.rs
@@ -1,14 +1,15 @@
 use cranelift::codegen::ir::*;
 use cranelift::codegen::isa::*;
 use cranelift::frontend::*;
+use cranelift::frontend::Variable;
 use cranelift::prelude::types::*;
 
-macro_rules! empty_new {
-    ($attr:ident) => {
+macro_rules! empty_namespace_invoke_return {
+    ($attr:ident, $func_name:ident, $invoke_name: ident, $ret: ident) => {
         paste::paste! {
             #[no_mangle]
-            pub extern "C" fn [<CL_ $attr _New >]() -> *mut [< $attr >] {
-                let mut val = [< $attr >]::new();
+            pub extern "C" fn [<CL_ $attr _ $func_name >]() -> *mut [< $ret >] {
+                let mut val = [< $attr >]::[<$invoke_name>]();
                 return &mut val;
             }
         }
@@ -22,6 +23,19 @@ macro_rules! empty_invoke_return {
             pub extern "C" fn [<CL_ $attr _ $name>](val: *mut $attr) -> *mut [< $ret >] {
                 let mut uval = unsafe { core::ptr::read(val) };
                 return &mut uval.[< $name >]();
+            }
+        }
+    }
+}
+
+macro_rules! one_invoke_void {
+    ($attr:ident, $name:ident, $one:ident) => {
+        paste::paste! {
+            #[no_mangle]
+            pub extern "C" fn [<CL_ $attr _ $name>](val: *mut $attr, val2: *mut $one) -> () {
+                let mut uval = unsafe { core::ptr::read(val) };
+                let uval2 = unsafe { core::ptr::read(val2) };
+                uval.[< $name >](uval2);
             }
         }
     }
@@ -47,18 +61,25 @@ macro_rules! type_new {
                 return &mut val;
             }
         }
-    }
-}
+    } }
+// Variable
+empty_dispose!(Variable);
 
+#[no_mangle]
+pub extern "C" fn CL_Variable_From_U32(val: u32) -> *mut Variable {
+    let mut val = Variable::from_u32(val);
+    return &mut val;
+}
 // ABIPARAM
 type_new!(AbiParam, I32, I32);
+empty_dispose!(AbiParam);
 
 // SIGNATURE
 type_new!(Signature, SystemV, CallConv::SystemV);
 empty_dispose!(Signature);
 
 // FUNCTION
-empty_new!(Function);
+empty_namespace_invoke_return!(Function, New, new, Function);
 empty_dispose!(Function);
 #[no_mangle]
 pub extern "C" fn CL_Function_With_Name_Signature(
@@ -74,6 +95,19 @@ pub extern "C" fn CL_Function_With_Name_Signature(
 // FUNCTION BUILDER
 empty_dispose!(FunctionBuilder);
 empty_invoke_return!(FunctionBuilder, create_block, Block);
+one_invoke_void!(FunctionBuilder, append_block_params_for_function_params, Block);
+
+#[no_mangle]
+pub extern "C" fn CL_FunctionBuilder_Declare_Var(
+    function: *mut FunctionBuilder,
+    var: *mut Variable,
+    ty: *mut Type,
+) -> () {
+    let ufunction = unsafe { &mut *function };
+    let uvar = unsafe { *var };
+    let uty = unsafe { *ty };
+    ufunction.declare_var(uvar, uty);
+}
 
 #[no_mangle]
 pub extern "C" fn CL_FunctionBuilder_New<'a>(
@@ -86,5 +120,5 @@ pub extern "C" fn CL_FunctionBuilder_New<'a>(
 }
 
 // FUNCTION BUILDER CONTEXT
-empty_new!(FunctionBuilderContext);
+empty_namespace_invoke_return!(FunctionBuilderContext, New, new, FunctionBuilderContext);
 empty_dispose!(FunctionBuilderContext);
--- src/cranelift-bindings.zig
+++ src/cranelift-bindings.zig
@@ -1,9 +1,31 @@
 const std = @import("std");
-pub const UserFuncName = opaque {};
-pub const FunctionStencil = opaque {};
-pub const FunctionParameters = opaque {};
 
-pub const Signature = opaque {};
+pub const UserFuncName = opaque {};
+pub const FunctionParameters = opaque {};
+pub const Block = opaque {};
+
+pub const Variable = opaque {
+    pub const new = CL_Variable_From_U32;
+    extern fn CL_Variable_From_U32() *Variable;
+
+    pub const dispose = CL_Variable_Dispose;
+    extern fn CL_Variable_Dispose(*Variable) void;
+};
+pub const AbiParam = opaque {
+    pub const new_i32 = CL_AbiParam_New_I32;
+    extern fn CL_AbiParam_New_I32() *AbiParam;
+
+    pub const dispose = CL_AbiParam_Dispose;
+    extern fn CL_AbiParam_Dispose(*AbiParam) void;
+};
+
+pub const Signature = opaque {
+    pub const new = CL_Signature_New_SystemV;
+    extern fn CL_Signature_New_SystemV() *Signature;
+
+    pub const dispose = CL_Signature_Dispose;
+    extern fn CL_Signature_Dispose(*Signature) void;
+};
 pub const Function = opaque {
     pub const named = cl_function_with_name_signature;
     extern fn cl_function_with_name_signature(*UserFuncName, *Signature) *Function;
@@ -16,22 +38,28 @@ pub const Function = opaque {
 };
 
 pub const FunctionBuilder = opaque {
-    pub const new = cl_function_builder_new;
-    extern fn cl_function_builder_new(*Function, *Context) *FunctionBuilder;
+    pub const new = CL_FunctionBuilder_New;
+    extern fn CL_FunctionBuilder_New(*Function, *Context) *FunctionBuilder;
+
+    pub const decl_var = CL_FunctionBuilder_Declare_Var;
+    extern fn CL_FunctionBuilder_Declare_Var(*FunctionBuilder, *Variable) void;
+
+    pub const create_block = CL_FunctionBuilder_create_block;
+    extern fn CL_FunctionBuilder_create_block(*FunctionBuilder, *Variable) void;
 
     pub const dispose = CL_FunctionBuilder_Dispose;
     extern fn CL_FunctionBuilder_Dispose(*FunctionBuilder) void;
 };
 
 pub const Context = opaque {
-    pub const new = cl_function_builder_context_new;
-    extern fn cl_function_builder_context_new() *Context;
+    pub const new = CL_FunctionBuilderContext_New;
+    extern fn CL_FunctionBuilderContext_New() *Context;
 
     pub const dispose = CL_FunctionBuilderContext_Dispose;
     extern fn CL_FunctionBuilderContext_Dispose(*Context) void;
 };
 
-test "should create and dispose context" {
+test "should create and dispose simple block function and context" {
     const context = Context.new();
     defer Context.dispose(context);
 
@@ -40,4 +68,13 @@ test "should create and dispose context" {
 
     const func_builder = FunctionBuilder.new(func, context);
     defer FunctionBuilder.dispose(func_builder);
+
+    const abi_param = AbiParam.new_i32();
+    defer AbiParam.dispose(abi_param);
+
+    const abi_param2 = AbiParam.new_i32();
+    defer AbiParam.dispose(abi_param2);
+
+    const block = FunctionBuilder.new_block(func_builder);
+    _ = block;
 }
