795f5942b21091781026945a7dbde97a1dafcb4e
Chris <chris.clark@coffeeb4code.com>
2024-02-04 21:06:56 -0600

    trying to figure out arrangement of slt, ast, linter, and who owns what

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -9,47 +9,6 @@ pub struct Lexeme {
     pub slice: String,
 }
 
-//#[derive(Debug, Clone, PartialEq)]
-//pub struct CodeLocation {
-//    pub val: String,
-//    pub line: usize,
-//    pub code: String,
-//    pub col: usize,
-//}
-//
-//impl<'s> CodeLocation {
-//    pub fn new(lexer: &Lexer<Token>) -> Self {
-//        let chars = lexer
-//            .source()
-//            .get(0..lexer.span().end)
-//            .unwrap_or_else(|| "");
-//        let val = lexer
-//            .source()
-//            .get(lexer.span().start..lexer.span().end)
-//            .unwrap_or_else(|| "(empty)");
-//        let mut lines = chars.split('\n').collect::<Vec<&str>>();
-//        let mut code = "".to_string();
-//        let mut col = 0;
-//        if lines.len() > 0 {
-//            code.push_str(lines.pop().unwrap());
-//            for x in lines.iter() {
-//                for _ in x.bytes() {
-//                    col += 1;
-//                }
-//            }
-//        } else {
-//            code.push_str(lexer.source());
-//        }
-//
-//        return CodeLocation {
-//            val: val.to_string(),
-//            line: lines.len() + 1,
-//            code,
-//            col: lexer.span().start - col - lines.len() + 1,
-//        };
-//    }
-////}
-
 pub struct TLexer<'s> {
     current: Option<Lexeme>,
     pub lexer: Lexer<'s, Token>,
--- linter/src/lib.rs
+++ linter/src/lib.rs
@@ -3,33 +3,40 @@ use codelocation::*;
 use lexer::*;
 use perror::LinterError;
 use perror::LinterErrorPoint;
+use token::Token;
 
 type ResultCheck = Result<Type, LinterError>;
 
 pub struct LintSource<'s> {
     buffer: &'s str,
+    pub issues: Vec<LinterError>,
 }
 
 #[derive(Debug)]
 pub enum Type {
-    F64(f64),
+    Num(u64),
+    Dec(f64),
 }
 
 impl<'s> LintSource<'s> {
     pub fn new(buffer: &'s str) -> Self {
-        LintSource { buffer }
+        LintSource {
+            buffer,
+            issues: vec![],
+        }
     }
-
-    pub fn type_check(&mut self, to_cmp: &Expr) -> ResultCheck {
+    pub fn type_check(&mut self, to_cmp: &mut Expr) -> ResultCheck {
         match to_cmp {
-            Expr::TopDecl(x) => self.type_check(&x.expr),
-            Expr::Number(x) => {
-                //if x.val.slice.parse::<f64>().is_ok() {
-                //    return Ok(Type::F64(x.val.slice.parse::<f64>().unwrap()));
-                //}
-                let err = LinterError::new("invalid range of f64".to_string());
-                self.update_error(err, "reduce scope of f64".to_string(), x.val.clone())
+            Expr::TopDecl(x) => {
+                let result = self.type_check(&mut x.expr)?;
+                return Ok(result);
             }
+
+            Expr::Number(x) => match x.val.token {
+                Token::Decimal => Ok(Type::Dec(x.val.slice.parse::<f64>().unwrap())),
+                Token::Num => Ok(Type::Num(x.val.slice.parse::<u64>().unwrap())),
+                _ => panic!("type-lang linter issue"),
+            },
             _ => panic!("type-lang linter issue"),
         }
     }
