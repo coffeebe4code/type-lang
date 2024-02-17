d4d82913f315f28a89e27ae0aa51159108307f22
Chris <chris.clark@coffeeb4code.com>
2024-02-17 15:34:04 -0600

    working on cache context

--- cachectx/src/lib.rs
+++ cachectx/src/lib.rs
@@ -1,34 +1,51 @@
-use ast::*;
+use ir::*;
 use lexer::*;
+use linter::*;
 use parser::*;
 use std::{fs::File, io::Read, path::PathBuf};
+use symtable::*;
+use types::*;
+
+struct InternalContext {
+    source: PathBuf,
+    parsed: Option<ResultExpr>,
+    symtable: Option<SymTable>,
+    tree: Option<TypeTree>,
+}
 
 pub struct CacheContext {
-    sources: Vec<PathBuf>,
-    parsers: Vec<Box<Expr>>,
-    errors: Vec<Box<Expr>>,
+    files: Vec<InternalContext>,
 }
 
 impl CacheContext {
     pub fn new() -> Self {
-        CacheContext {
-            sources: vec![],
-            parsers: vec![],
-            errors: vec![],
-        }
+        CacheContext { files: vec![] }
     }
     pub fn add_file(&mut self, source: PathBuf) -> () {
-        self.sources.push(source);
+        self.files.push(InternalContext {
+            source,
+            parsed: None,
+            symtable: None,
+            tree: None,
+        });
     }
-    pub fn take_parsed(&mut self) -> () {
-        self.sources.iter().for_each(|s| {
-            let mut file = File::open(s).unwrap();
+    pub fn analysis(&mut self) -> () {
+        self.files.iter_mut().enumerate().for_each(|(_i, ic)| {
+            let mut file = File::open(&ic.source).unwrap();
             let mut contents = String::new();
             file.read_to_string(&mut contents).unwrap();
             let lexer = TLexer::new(&contents);
             let mut parser = Parser::new(lexer);
-            let result = parser.all().unwrap();
-            self.parsers.push(result);
+            match parser.all() {
+                Ok(mut val) => {
+                    let mut sym = SymTable::new();
+                    let mut linter = LintSource::new(&contents, &mut sym);
+
+                    let analysis = linter.type_check(&mut val);
+                    ic.symtable = Some(sym);
+                }
+                Err(perr) => ic.parsed = Some(Err(perr)),
+            }
         })
     }
 }
