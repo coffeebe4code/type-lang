130be45de162c32617ea27fe2cf1219df6836027
Chris <chris.clark@coffeeb4code.com>
2024-12-26 22:19:00 -0600

    playing around with representations in rust

---
+++ repr/src/lib.rs
@@ -0,0 +1,16 @@
+#[no_mangle]
+pub extern "C" fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[no_mangle]
+pub fn make_binop(thing: u64, small: char, big: u64) -> BinOp {
+    return BinOp { thing, small, big };
+}
+
+#[repr(C)]
+pub struct BinOp {
+    thing: u64,
+    small: char,
+    big: u64,
+}
