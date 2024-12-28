9aa0b0bf14bdcbf093ceb8e18ca05f0be20f2348
Chris Clark <78776922+coffeebe4code@users.noreply.github.com>
2024-12-28 11:08:05 -0600

    Repr rust (#35)
    * added all the pieces for repr rust comparisons

---
+++ bootstrap/repr/integration.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+uint64_t add(uint64_t left, uint64_t right) {
+  return left + right;
+}
+
---
+++ bootstrap/repr/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern uint64_t add(uint64_t, uint64_t);
+
+int main() {
+  printf("Hello");
+  printf("Hello %lu", add(2, 7));
+  puts("Hello");
+}
