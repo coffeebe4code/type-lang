de17a9bcf9ea04b4461435fc37f3f8bc59daafd5
Chris Clark <78776922+coffeebe4code@users.noreply.github.com>
2025-01-08 22:07:36 -0600

    i fixed the params to fir (#41)

--- bootstrap/repr/main.c
+++ bootstrap/repr/main.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 extern uint64_t add(uint64_t, uint64_t);
 
 int main() {
-  printf("Hello");
-  printf("Hello %lu", add(2, 7));
-  puts("Hello");
+  uint64_t val = add(2, 7);
+  printf("Hello %lu\n", val);
+  return val;
 }
